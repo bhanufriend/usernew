49ed78f26e800b48f7bb9b8f17d175e899c31a0d
bhanufriend <167551354+bhanufriend@users.noreply.github.com>
2024-04-19 23:49:56 -0500

    Add files via upload

---
+++ api/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://akulabhanuprasadreddy:Bhanu12@cluster0.daenusd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    options: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+};
